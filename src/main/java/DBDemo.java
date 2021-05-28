e169b3e0e3e992e0f447a5d9d9e8d8a38dfb52d1
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-05-29 01:35:02 +0530

    Connected to database

---
+++ src/main/java/DBDemo.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+public class DBDemo {
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String username = "root";
+        String password = "1111";
+        Connection connection;
+        try {
+            System.out.println("connecting to database");
+            connection = DriverManager.getConnection(jdbcURL,username,password);
+            System.out.println("Connected Successfully!");
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver Loaded");
+        }catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        listDrivers();
+    }
+
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println(" "+driverClass.getClass().getName());
+        }
+    }
+}
